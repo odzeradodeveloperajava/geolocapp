7587e54b4cebf2bfe8c1996e3327db3c36e6a192
slawo tuszko <odzeradodeveloperajava@gmail.com>
2021-05-02 20:14:24 +0200

    custom input button

--- src/components/upoloadHandler/uploadHandler.js
+++ src/components/upoloadHandler/uploadHandler.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './uploadHandler.css';
 
-const UploadHandler = ({submitFn}) =>(
-    <form onSubmit={submitFn}>
-    	<div className="uploadHandlerWrapper">
-			<i className="far fa-folder-open fa-5x"></i>
-			<span>Drag&Drop files</span>
-			<span>or</span>
-			<div className="uploadTextContainer">
-				<button id="browseFilesBtn" className="browseFilesButton">Browse Files</button>
+const UploadHandler = ({submitFn}) =>{
+
+	const inputFile = useRef(null)
+
+	const onButtonClick = (e) => {
+		e.preventDefault();
+		inputFile.current.click();
+
+	}
+
+	return (
+	    <form onSubmit={submitFn}>
+	    	<div className="uploadHandlerWrapper">
+				<i className="far fa-folder-open fa-5x"></i>
+				<span>Drag&Drop files</span>
+				<span>or</span>
+				<div className="uploadTextContainer">
+					<button onClick={onButtonClick} id="browseFilesBtn" className="browseFilesButton">Browse Files</button>
+				</div>
+	    		<div className="inputSpace">
+					<input ref={inputFile} id="input" type="file" multiple  accept="image/*" onChange={submitFn}/>
+				</div>
 			</div>
-    		<div className="inputSpace">
-				<input id="input" type="file" multiple  accept="image/*" onChange={submitFn}/>
-			</div>
-		</div>
-	</form>
-);
+		</form>
+	)
+};
 
 
 
