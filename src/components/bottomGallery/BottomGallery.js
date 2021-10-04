b20e06b02aab8d994698efd6f3f71e6bff451446
Slawo <odzeradodeveloperajava@gmail.com>
2021-10-04 09:06:12 +0200

    strugling with firebase download

--- src/components/bottomGallery/BottomGallery.js
+++ src/components/bottomGallery/BottomGallery.js
@@ -1,14 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
+
+const storage = getStorage();
+const listRef = ref(storage, 'images');
 
 
+class BottomGallery extends React.Component {
 
-const BottomGallery = (data) => {
-    console.log(data)
+    state = {
+        items: [],
+    }
+
+    getDownloadUrl = () => {
+        listAll(listRef)
+        .then((res) => {
+        res.items.forEach((itemRef) => {
+          const fileRef = itemRef._location.path;
+           getDownloadURL(ref(storage, fileRef)).then((url) =>{
+                this.setState((prevState) => ({
+                items: [...prevState.items, url],
+              }));
+        })
+        });
+        }).catch((error) => {
+        console.log(error);
+        });
+    }
+
+    fetchFiles = () => {
+        
+        fruits.forEach(myFunction);
+    }
+
+render() {
     return (
-        <div>
-            xx
-        </div>
+        <div>xx</div>
     )
 }
 
+
+
+
+}
 export default BottomGallery;
--- src/functions/firebaseHandler/firebaseDownloadHandler.js
+++ src/functions/firebaseHandler/firebaseDownloadHandler.js
@@ -1,33 +1,14 @@
-import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
-import React, { useState } from 'react';
+import React from 'react'
 
-const storage = getStorage();
-const listRef = ref(storage, 'images');
-
-const FirebaseDownloadHandler = async () => {
-
-const [state, setState] = useState([]);
-
-
-    listAll(listRef)
-        .then((res) => {
-            res.items.forEach((itemRef) => {
-              const fileRef = itemRef._location.path;
-               getDownloadURL(ref(storage, fileRef)).then((url) =>{
-                this.setState((prevState) => ({
-                  urls: [...prevState.urls, url],
-                }));
-            })
-        });
-     }).catch((error) => {
-    console.log(error);
-    });
-  }
+const firebaseDownloadHandler = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
 
-
-console.log('bla')
-
-export default FirebaseDownloadHandler;
+export default firebaseDownloadHandler;
 
 
