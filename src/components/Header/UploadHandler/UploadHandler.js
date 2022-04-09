39991cd0d5355f56dc416eb2704b08c3052d6797
slawo tuszko <odzeradodeveloperajava@gmail.com>
2022-04-09 17:04:35 +0200

    separating logic from uploadHandler.js component

--- src/components/Header/UploadHandler/UploadHandler.js
+++ src/components/Header/UploadHandler/UploadHandler.js
@@ -1,66 +1,23 @@
 import React, {useRef} from 'react';
 import './uploadHandler.css';
-import EXIF from 'exif-js';
-import firebaseUploadHandler from '../../../functions/firebaseScripts/firebaseUploadHandler';
-import returnNewItem from './../../../functions/returnNewItem/returnNewItem'
-import { connect } from 'react-redux';
-import { resetStateValue, deleteActiveItems, countFilesToProcess, fileProcessedAdder, putNamesNoExif, addActiveFile, setCenterPosition, setActiveCardNr} from '../../../actions';
-import { areCoordinatesValid } from '../../../functions/areCoordinatesValid/areCoordinatesValid';
+import addItemHandler from '../../../functions/addItemHandler/addItemHandler';
 
-const UploadHandler = ({resetStateValueHandler, countFilesToProcessHandler, deleteActiveItemsHandler, fileProcessedHandler, addActiveFileHandler, setCenterPositionHandler, setActiveCardNrHandler, putNamesNoExifHandler}) =>{
+const UploadHandler = () =>{
 	const inputFile = useRef(null)
 	const onButtonClick = (e) => {
 		e.preventDefault();
 		inputFile.current.click();
 	}
-	const addItem = (e) => {
-		let filesArray = e.target.files.length;
-		resetStateValueHandler('filesToProcess', 0);
-		resetStateValueHandler('fileProcessed', 0);
-		countFilesToProcessHandler(filesArray)
-		deleteActiveItemsHandler();
-		for (let i = 0; i < filesArray; i++) {
-		  const selectedFile = e.target.files[i];
-		  EXIF.getData(selectedFile, async function () {
-			if (areCoordinatesValid(this.exifdata.GPSLatitude, this.exifdata.GPSLongitude) === true) {
-			  firebaseUploadHandler(selectedFile);
-			  const fileMetadata = await returnNewItem(selectedFile);
-				addActiveFileHandler(fileMetadata);
-				setCenterPositionHandler(fileMetadata.lat.toFixed(3), fileMetadata.lon.toFixed(3))
-				setActiveCardNrHandler(0)
-			} else {
-				fileProcessedHandler()
-				putNamesNoExifHandler(selectedFile.name)
-		  	}
-		})}
-	}
+
 	return (
-	    <form onSubmit={addItem}>
+	    <form onSubmit={addItemHandler}>
 					<button onClick={onButtonClick} id="browseFilesBtn" className="browseFilesButton">Browse Files</button>
-					<input ref={inputFile} id="input" type="file" multiple  accept="image/*" onChange={addItem}/>
+					<input ref={inputFile} id="input" type="file" multiple  accept="image/*" onChange={addItemHandler}/>
 		</form>
 	)
 };
 
-const mapStateToProps = state =>{
-    return {
-        filesToProcess: state.filesToProcess,
-        fileProcessed: state.fileProcessed,
-        activeFiles: state.activeItems
-    }
-}
-
-const mapDispatchToProps = dispatch =>({
-    countFilesToProcessHandler: (howManyFiles) => dispatch(countFilesToProcess(howManyFiles)),
-	deleteActiveItemsHandler: () => dispatch(deleteActiveItems()),
-	fileProcessedHandler: () => dispatch(fileProcessedAdder()),
-	putNamesNoExifHandler: (name) => dispatch(putNamesNoExif(name)),
-	addActiveFileHandler: (file) => dispatch(addActiveFile(file)),
-	setCenterPositionHandler: (lat, lng) => dispatch(setCenterPosition(lat, lng)),
-	setActiveCardNrHandler: (number) => dispatch(setActiveCardNr(number)),
-	resetStateValueHandler: (sname, value) => dispatch(resetStateValue(sname, value))
-})
 
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(UploadHandler);
+export default UploadHandler;
---
+++ src/functions/addItemHandler/addItemHandler.js
@@ -0,0 +1,34 @@
+import EXIF from 'exif-js';
+import firebaseUploadHandler from '../firebaseScripts/firebaseUploadHandler';
+import returnNewItem from '../returnNewItem/returnNewItem';
+import { areCoordinatesValid } from '../areCoordinatesValid/areCoordinatesValid';
+import { resetStateValue, deleteActiveItems, countFilesToProcess, fileProcessedAdder, putNamesNoExif, addActiveFile, setCenterPosition, setActiveCardNr} from '../../actions';
+import store from '../../store';
+
+const addItemHandler = (e) => {
+
+console.log(e)
+
+		let filesArray = e.target.files.length;
+        console.log(e.target.files)
+		store.dispatch(resetStateValue('filesToProcess', 0));
+		store.dispatch(resetStateValue('fileProcessed', 0));
+		store.dispatch(countFilesToProcess(filesArray));
+		store.dispatch(deleteActiveItems());
+		for (let i = 0; i < filesArray; i++) {
+		  const selectedFile = e.target.files[i];
+		  EXIF.getData(selectedFile, async function () {
+			if (areCoordinatesValid(this.exifdata.GPSLatitude, this.exifdata.GPSLongitude) === true) {
+			  firebaseUploadHandler(selectedFile);
+			  const fileMetadata = await returnNewItem(selectedFile);
+				store.dispatch(addActiveFile(fileMetadata));
+				store.dispatch(setCenterPosition(fileMetadata.lat.toFixed(3), fileMetadata.lon.toFixed(3)))
+				store.dispatch(setActiveCardNr(0))
+			} else {
+				store.dispatch(fileProcessedAdder())
+				store.dispatch(putNamesNoExif(selectedFile.name))
+		  	}
+		})}
+}
+
+export default addItemHandler
