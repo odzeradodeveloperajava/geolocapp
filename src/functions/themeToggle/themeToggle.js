f4df49341fbe50e0cbfcbf3831f00f40ef480218
slawo tuszko <odzeradodeveloperajava@gmail.com>
2022-04-09 17:06:38 +0200

    deleting debugging console logs

--- src/functions/themeToggle/themeToggle.js
+++ src/functions/themeToggle/themeToggle.js
@@ -4,11 +4,9 @@ import { toggleTheme } from '../../actions';
 
 export const themeToggle = () => {
     if(store.getState().activeTheme === 'lightTheme'){
-        console.log('switched to black')
         store.dispatch(toggleTheme('darkTheme'))
     }
     else{
         store.dispatch(toggleTheme('lightTheme'));
-        console.log('switched to light')
     }
 }
