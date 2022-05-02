d844d5423fe2f8fef23fc57205b1b5718b8cd7ac
slawo tuszko <odzeradodeveloperajava@gmail.com>
2022-05-02 17:58:10 +0200

    refactor

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[See live here](https://exifhost.web.app/)
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## GeolocApp
--- src/functions/areCoordinatesValid/areCoordinatesValid.js
+++ src/functions/areCoordinatesValid/areCoordinatesValid.js
@@ -1,10 +1,15 @@
 export const areCoordinatesValid = (lat, lon) => {
     const areValid = () => {
+      if(lat && lon !== undefined){
         if (lat[0].numerator >= (-90) && lat[0].numerator <= (90) ){
             if (lon[0].numerator >= (-180) && lon[0].numerator <= (180) ){
                 return true
             }
             }}
+        else{
+          return false
+        }
+    }
   return (
     areValid()
   )
